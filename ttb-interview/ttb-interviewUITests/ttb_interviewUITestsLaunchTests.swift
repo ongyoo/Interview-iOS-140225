deb644ecc7195c2cccd194a3b17d433635f2c1fa
Komsit <ongyoo53@gmail.com>
2025-02-14 11:04:10 +0700

    [add] file project

---
+++ ttb-interview/ttb-interviewUITests/ttb_interviewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ttb_interviewUITestsLaunchTests.swift
+//  ttb-interviewUITests
+//
+//  Created by Komsit Chusangthong on 2/14/25.
+//
+
+import XCTest
+
+final class ttb_interviewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
